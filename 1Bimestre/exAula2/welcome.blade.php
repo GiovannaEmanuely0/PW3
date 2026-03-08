5c3c175865d195c01b3508c317a6b2eba41534f4
Giovanna Emanuely <giovannaEmma16@gmail.com>
2026-03-08 13:27:16 -0300

    Add files via upload

---
+++ 1Bimestre/exAula2/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{url('/css/style.css')}}">
+</head>
+<body>
+    <h1> Forget me not </h1>
+    <div>
+        <img class="image" src="{{url('/image/flor.jfif')}}"/>
+    </div>
+
+</body>
+</html>
